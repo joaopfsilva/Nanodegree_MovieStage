c626eb2748840b24ac893a6d8ff972c3b69876aa
joaopfsilva <j0a0.silva.kle@gmail.com>
2016-03-07 23:45:02 +0000

    added layout improvement added second activity

--- app/src/main/java/com/example/joaopfsilva/moviestage1/DetailMovie.java
+++ app/src/main/java/com/example/joaopfsilva/moviestage1/DetailMovie.java
@@ -1,23 +1,13 @@
 package com.example.joaopfsilva.moviestage1;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import info.movito.themoviedbapi.model.*;
 
 public class DetailMovie extends AppCompatActivity {
     private String API_KEY = "***";
